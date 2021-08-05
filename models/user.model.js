ae2535fa5b9d6e379da32fde9152c5fa9042772d
Shane Umayanga <shaneumayanga445@gmail.com>
2021-08-05 21:09:24 +0530

    user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,22 @@
-const sequalize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 
 
 
+module.exports = (Conn)=>{
+    return Conn.define("User",{
+        UserID:{
+            type: DataTypes.STRING,
+            primaryKey:true
+        },
+        Email :{
+            type: DataTypes.STRING,
+        },
+        Password :{
+            type: DataTypes.STRING
+        }
+    })
+}
 
 
 
